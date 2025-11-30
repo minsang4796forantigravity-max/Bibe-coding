aae5909bd3e78f65c72a4de619a6ef38160d701a
minsang <minsang4796forAntigravity@gmail.com>
2025-11-30 20:06:44 +0900

    Fix AI single player mode with difficulty levels

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,10 +1,10 @@
 import { io } from "socket.io-client";
 
-// 로컬 개발용
-const URL = "http://localhost:3000";
+// 배포용 - Render 서버
+const URL = "https://bibe-coding.onrender.com";
 
-// 배포용 (주석 처리)
-// const URL = "https://bibe-coding.onrender.com";
+// 로컬 개발용 (주석 처리)
+// const URL = "http://localhost:3000";
 
 export const socket = io(URL, {
   autoConnect: false
